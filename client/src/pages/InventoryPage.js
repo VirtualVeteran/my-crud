62eb4069ec62f9b1a513ff17e519dfc25f9b10bc
virtualveteran <estheroswald001@yahoo.com>
2025-01-16 16:48:35 -0700

    creates knex dependencies, knexfile.js, migrations tables

--- client/src/pages/InventoryPage.js
+++ client/src/pages/InventoryPage.js
@@ -3,13 +3,13 @@ import React from 'react';
 const InventoryPage = () => (
   <div style={{ backgroundColor: '#A8D8FF', padding: '2rem' }}>
     <header>
-      <nav>
-        <a href="/">Home</a>
-        <a href="/add-item">Add Item</a>
-        <a href="/logout">Logout</a>
-      </nav>
-    </header>
     <h1>Your Inventory</h1>
+    <div className='button-container'>
+      <a href="/" className="kawaii-button">Home</a>
+      <a href="/add-item" className="kawaii-button">Add Item</a>
+      <a href="/logout" className="kawaii-button">logout</a>
+      </div>
+    </header>
     <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '1rem' }}>
       {/* Render items here */}
       <div className="item-card">
