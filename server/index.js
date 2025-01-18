7a9eeec316932f8727494810164b2f4251578c0c
virtualveteran <estheroswald001@yahoo.com>
2025-01-17 17:46:05 -0700

    changes express server to 5000

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const port = 8080;
+const port = 5000;
 
 app.get('/', (req, res) => {
     res.status(200).json('hello from root route')
