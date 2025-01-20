c4624002f7bf73a5be5c34186141c4dcb6931055
virtualveteran <estheroswald001@yahoo.com>
2025-01-19 17:15:08 -0700

    adds developer bio

--- client/src/pages/DeveloperPage.js
+++ client/src/pages/DeveloperPage.js
@@ -13,19 +13,21 @@ const DeveloperPage = () => {
   className="developer-avatar" 
 />    <h1 className="developer-name">🌸Hi, I'm E! 🌸</h1>
         <p className="developer-bio">
-          I'm a passionate web developer who loves creating magical experiences on the web.✨ When I'm not coding, you can find me:
+        🪐💫I am a Developer for Mission Delta 4 and I design and build web apps to support Space Operations World Wide💫🪐 </p>
+          
+         <p>  When I'm not coding, you can find me:
         </p>
         <ul className="hobbies-list">
-          <li>🎨 Drawing and designing cute characters</li>
-          <li>🎮 Playing cozy video games</li>
-          <li>☁️ Dreaming up new ideas for fun projects</li>
-          <li>🐾 Hanging out with my cats</li>
+          <li>🎨 Drawing and designing cozy stickers on my Redbubble account</li>
+          <li>🎮 Playing cozy video games like - Palia, Fairy Farm, Wylde Flowers, Infinity Niki, Witch Cove</li>
+          <li>☁️ Being whimsical outside with my Spouse</li>
+          <li>🐾 Hanging out with my cats- Aspen, Kivi, Shook, and Magnus</li>
         </ul>
         <p className="developer-mission">
-          My mission? To spread joy and a touch of kawaii through every website I build! 💕
+        🌟My mission? To get my Z-prefix!🌟
         </p>
         <div className="social-links">
-         
+        <a href="/" className="kawaii-button">Back to Home</a>
         </div>
       </div>
     </div>
